3d02e86f937be50dcee990515e8c99c4739964fc
jjortiz016 <jjortiz016@misena.edu.co>
2024-04-29 14:02:38 -0500

    Se programó clase entity Role.java.

--- src/main/java/com/jhon/tiendaback/models/Role.java
+++ src/main/java/com/jhon/tiendaback/models/Role.java
@@ -1,4 +1,26 @@
 package com.jhon.tiendaback.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID roleId=UUID.randomUUID();
+
+    @Column(nullable = false,length =50)
+    private String nameRole;
+
+    @Column(nullable = false,length = 100)
+    private String descriptionRole;
 }
