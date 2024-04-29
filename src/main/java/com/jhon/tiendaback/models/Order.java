3e1a8fae5fb40f632aa9da7f8e9b6544a7dc25b7
jjortiz016 <jjortiz016@misena.edu.co>
2024-04-29 13:08:16 -0500

    Se programó clase entity Order.java y se crearon clase vacias Payment y User.

--- src/main/java/com/jhon/tiendaback/models/Order.java
+++ src/main/java/com/jhon/tiendaback/models/Order.java
@@ -1,4 +1,39 @@
 package com.jhon.tiendaback.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="order")
 public class Order {
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
+    @Column(unique=true, name="orderID")
+    private UUID orderID= UUID.randomUUID();
+
+    @Column(length = 50)
+    private Double mount;
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderDetail> orderDetails;
+
+    @OneToOne
+    @JsonIgnore
+    private Payment payment;
+
+
 }
