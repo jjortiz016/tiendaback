fc02a2bed02eb09a4cc87c8999f4ebe660e88142
jjortiz016 <jjortiz016@misena.edu.co>
2024-04-29 07:55:53 -0500

    se programo la clase OrderDetail y creacion de la clase vacia Order.

--- src/main/java/com/jhon/tiendaback/models/OrderDetail.java
+++ src/main/java/com/jhon/tiendaback/models/OrderDetail.java
@@ -1,4 +1,35 @@
 package com.jhon.tiendaback.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+@Table(name="order-details")
 public class OrderDetail {
+    @Id
+    @Column(name="orderDetailID")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID orderDetailID=UUID.randomUUID();
+
+    @JoinColumn(name="orderId")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name="productID")
+    private Product product;
+
+    private Integer quantity;
+
+    private Double price;
+
 }
