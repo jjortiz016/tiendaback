eff7a4135f2052b03f652ff8e303b7a2a355f6a3
jjortiz016 <jjortiz016@misena.edu.co>
2024-04-29 06:49:07 -0500

    Creacion de la clase vacia OrderDetail y programación de la clase product.

--- src/main/java/com/jhon/tiendaback/models/Product.java
+++ src/main/java/com/jhon/tiendaback/models/Product.java
@@ -1,9 +1,20 @@
 package com.jhon.tiendaback.models;
 
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.List;
 import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 @Entity
 @Table(name="products")
@@ -12,8 +23,21 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.UUID) //uuid para generar codigo unico de forma aleatorioa teniendo en cuenta el tiempo dia, segundos
     @Column(name="productId")
     private UUID productId= UUID.randomUUID();
+
+    @Column(nullable = false, length = 50)
     private String nameProduct;
+
+    @Column(nullable = false, length = 100)
+    private String descriptionProduct;
+
+    @Column(nullable = false, length = 50)
     private Double priceProduct;
+
+    @ManyToOne
+    private Category category;
+
+    @OneToMany(mappedBy = "product")
+    private List<OrderDetail> orderDetails;
 
 
 }
