f1c829157701b44177dc20ab1bad1119ae6a36f4
jjortiz016 <jjortiz016@misena.edu.co>
2024-04-29 13:23:50 -0500

    Se programó la clase entity Payment.java.

--- src/main/java/com/jhon/tiendaback/models/Payment.java
+++ src/main/java/com/jhon/tiendaback/models/Payment.java
@@ -1,4 +1,38 @@
 package com.jhon.tiendaback.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="payments")
+
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "paymentId")
+    private UUID paymentId = UUID.randomUUID();
+
+    @Column(nullable = false, length = 50)
+    private String nOperation;
+
+
+    @Column(length = 50)
+    private Double mount;
+
+    @Column(nullable = false, length = 50)
+    private Date datePayment;
+
+    @OneToOne
+    private Order order;
+
 }
