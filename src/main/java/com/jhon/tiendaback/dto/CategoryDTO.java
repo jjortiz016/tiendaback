a4835ac6fcb0c67c1abcce96de9024d20a13218a
jjortiz016 <jjortiz016@misena.edu.co>
2024-05-05 17:02:29 -0500

    Se programó la clase Product y la clase ProductDTO.

--- src/main/java/com/jhon/tiendaback/dto/CategoryDTO.java
+++ src/main/java/com/jhon/tiendaback/dto/CategoryDTO.java
@@ -1,5 +1,6 @@
 package com.jhon.tiendaback.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -21,9 +22,9 @@ public class CategoryDTO {
     @NotEmpty
     private String nameCategory;
 
-
     @NotNull
-    @JsonManagedReference
+    @JsonManagedReference //se utiliza en clases de modelo de Java para manejar relaciones bidireccionales y evitar problemas de recursión infinita al serializar objetos JSON.se coloca en la clase padre
+   // @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) //ignorar atributos hibernate para que no genere error en el json
     private List<ProductDTO> products;
 
 
--- src/main/java/com/jhon/tiendaback/dto/ProductDTO.java
+++ src/main/java/com/jhon/tiendaback/dto/ProductDTO.java
@@ -1,4 +1,39 @@
 package com.jhon.tiendaback.dto;
 
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.jhon.tiendaback.models.OrderDetail;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class ProductDTO {
+    @EqualsAndHashCode.Include
+    private UUID productId;
+
+    @NotEmpty
+    private String nameProduct;
+
+    @NotEmpty
+    private String descriptionProduct;
+
+    @NotEmpty
+    private Double priceProduct;
+
+    @NotNull
+    private CategoryDTO category;
+
+
+    @JsonManagedReference
+    @NotNull
+    private List<OrderDetail> orderDetails;
+
 }
